eb0902756d31ee5ddfc8b7d21574040b41cbfd47
luqmanulhakim666 <luqmanulh25@gmail.com>
2024-11-29 12:21:46 +0700

    gmail

--- pages/google/callback.vue
+++ pages/google/callback.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    <general-loading />
+    <general-loading v-if="state.isLoading" />
+
+    <v-dialog width="500" v-model="state.isDialog">
+      <div
+        class="white pa-6 rounded-xl d-flex flex-column align-center justify-center"
+      >
+        <v-icon size="62" color="error">mdi-close-octagon</v-icon>
+        <h5 class="text-center h5--small dark--text text-lighten-4 mt-8">
+          Email Business tidak diizinkan
+        </h5>
+        <h5 class="text-center h5--small dark--text text-lighten-4">
+          Harap login dengan akun @gmail.com
+        </h5>
+
+        <v-btn
+          @click="goBack()"
+          block
+          depressed
+          class="mt-8 secondary text-capitalize h7--xxsmall"
+          >Kembali</v-btn
+        >
+      </div>
+    </v-dialog>
   </div>
 </template>
 
 <script>
 import meta from "@/mixins/meta";
+import { state } from "../../store/auth";
 const Cookie = process.client ? require("js-cookie") : undefined;
 
 export default {
@@ -13,11 +36,16 @@ export default {
   mixins: [meta],
 
   data: () => ({
+    state: {
+      isLoading: false,
+      isDialog: false,
+    },
     meta: {
       title: "Google Verifiying",
     },
   }),
   async mounted() {
+    this.state.isLoading = true;
     const hash = window.location.hash.substr(1);
     const result = hash.split("&").reduce((res, item) => {
       const parts = item.split("=");
@@ -34,7 +62,14 @@ export default {
           },
         }
       );
+
       const userInfo = await response.json();
+
+      if (!userInfo?.email?.includes("@gmail.com")) {
+        this.state.isDialog = true;
+        this.state.isLoading = false;
+        return;
+      }
 
       await this.$store.dispatch("auth/setGoogleToken", result.access_token);
       await Cookie.set("googleProfile", JSON.stringify(userInfo));
@@ -43,6 +78,14 @@ export default {
     } catch (error) {
       console.error("Error fetching user info:", error);
     }
+
+    this.state.isLoading = false;
+  },
+
+  methods: {
+    goBack() {
+      window.location.href = Cookie.get("googleCallback");
+    },
   },
 };
 </script>
